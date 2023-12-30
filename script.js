d72363fc7b0a9ee405a88d3fbdc78d86e79fbbcc
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-30 12:01:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const sizeDisplay = document.getElementById('sizeDisplay');
+
+function updateSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    sizeDisplay.textContent = `Width: ${width} Height: ${height}`;
+}
+
+// Initial size update
+updateSize();
+
+// Update size on window resize
+window.addEventListener('resize', updateSize);
